9a81989a660e751ea0932b547a9a941261be8926
MRizainF <icon.firdaus@gmail.com>
2024-12-12 14:24:17 +0700

    feat: update main

--- main.py
+++ main.py
@@ -1,97 +1,51 @@
-from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QToolBar, QPushButton, QWidget, QHBoxLayout, 
-    QSpacerItem, QSizePolicy, QVBoxLayout, QLabel, QStackedWidget, QFrame
-)
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
-from src.gui.navbar import Navbar
-from src.gui.home_section import HomeSection
-from src.gui.journal_log_section import JournalLogSection
-from src.gui.bucket_list_section import BucketListSection
-#from src.gui.stat_section import StatSection
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
+from kivy.graphics import Color, Rectangle
+from src.components.sidebar import Sidebar
+from src.boundary.homePage import HomePage
+from src.boundary.journalPage import JournalPage
+from src.boundary.bucketListPage import BucketListPage
+from src.boundary.page3 import Page3
+from src.boundary.page4 import Page4
+from src.boundary.formJournalPage import FormJournalPage  # Import FormJournalPage
+from src.boundary.formBucketPage import FormBucketPage  # Import FormJournalPage
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("VACATOOLS")
+class MainApp(App):
+    def build(self):
+        root = BoxLayout(orientation='horizontal')
 
-        # Set the window size
-        self.setGeometry(200, 200, 300, 300)
+        # Screen manager to handle navigation
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(HomePage(name='HOME'))
+        screen_manager.add_widget(JournalPage(name='JOURNAL_LOG'))
+        screen_manager.add_widget(FormJournalPage(name='FORM_JOURNAL'))  
+        screen_manager.add_widget(FormBucketPage(name='FORM_BUCKET'))  
+        screen_manager.add_widget(BucketListPage(name='BUCKET_LIST'))
+        screen_manager.add_widget(Page3(name='STATISTIC'))
+        screen_manager.add_widget(Page4(name='LOCATION'))
 
-        # Navbar
-        self.navbar = Navbar(self)
-        self.addToolBar(Qt.TopToolBarArea, self.navbar)
+        # Add white background to the screen manager
+        with screen_manager.canvas.before:
+            Color(1, 1, 1, 1)
+            self.bg_rect = Rectangle(size=screen_manager.size, pos=screen_manager.pos)
 
-        # Connect buttons to respective section functions
-        self.navbar.buttons[0].clicked.connect(self.show_home)
-        self.navbar.buttons[1].clicked.connect(self.show_journal_log)
-        self.navbar.buttons[2].clicked.connect(self.show_bucket_list)
-        self.navbar.buttons[3].clicked.connect(self.show_statistic)
-        self.navbar.buttons[4].clicked.connect(self.show_location)
+        # Bind size and position to update background accordingly
+        screen_manager.bind(size=self._update_bg, pos=self._update_bg)
 
-        # Create the central widget for the main content area (QStackedWidget for dynamic content change)
-        self.central_widget = QStackedWidget()
-        self.setCentralWidget(self.central_widget)
+        # Sidebar for navigation
+        sidebar = Sidebar(screen_manager=screen_manager)
 
-        # Create sections
-        self.home_section = HomeSection()
-        self.journal_log_section = JournalLogSection()
-        self.bucket_list_section = BucketListSection()
-        #self.statistic_section = StatSection()
-        self.location_section = QWidget()
+        # Add sidebar and screen manager to the root layout
+        root.add_widget(sidebar)
+        root.add_widget(screen_manager)
 
-        # Set up each section
-        # self.setup_home_section()
-        # self.setup_journal_log_section()
-        #self.setup_bucket_list_section()
-        #self.setup_statistic_section()
-        self.setup_location_section()
+        return root
 
-        # Add the sections to the stacked widget
-        self.central_widget.addWidget(self.home_section)
-        self.central_widget.addWidget(self.journal_log_section)
-        self.central_widget.addWidget(self.bucket_list_section)
-        #self.central_widget.addWidget(self.statistic_section)
-        self.central_widget.addWidget(self.location_section)
-
-        # Show home section by default
-        self.show_home()
-
-    def setup_location_section(self):
-        """Setup content for the location section"""
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("Welcome to the Location Section!"))
-        self.location_section.setLayout(layout)
-
-    def show_home(self):
-        """Show the home section"""
-        self.central_widget.setCurrentWidget(self.home_section)
-        self.setWindowTitle("VACATOOLS - Beranda")
-
-    def show_journal_log(self):
-        """Show the journal log section"""
-        self.central_widget.setCurrentWidget(self.journal_log_section)
-        self.setWindowTitle("VACATOOLS - Jurnal Log")
-
-    def show_bucket_list(self):
-        """Show the bucket list section"""
-        self.central_widget.setCurrentWidget(self.bucket_list_section)
-        self.setWindowTitle("VACATOOLS - Bucket List")
-
-    def show_statistic(self):
-        """Show the statistic section"""
-        self.central_widget.setCurrentWidget(self.statistic_section)
-        self.setWindowTitle("VACATOOLS - Statistik")
-
-    def show_location(self):
-        """Show the location section"""
-        self.central_widget.setCurrentWidget(self.location_section)
-        self.setWindowTitle("VACATOOLS - Tambah Lokasi")
-
+    def _update_bg(self, instance, *args):
+        """Update the background size and position of the ScreenManager."""
+        self.bg_rect.size = instance.size
+        self.bg_rect.pos = instance.pos
 
 if __name__ == '__main__':
-    import sys
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+    MainApp().run()
