2c09328442f5ac6b177bcda2f81f94db690ae111
MRizainF <icon.firdaus@gmail.com>
2024-12-14 20:35:16 +0700

    feat: add reload button

--- src/components/sidebar.py
+++ src/components/sidebar.py
@@ -2,7 +2,10 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Rectangle
+from kivy.app import App
 from img.font.fontManager import FontManager
+import sys
+import subprocess
 
 class Sidebar(BoxLayout):
     def __init__(self, screen_manager, **kwargs):
@@ -57,6 +60,19 @@ class Sidebar(BoxLayout):
         for button_data in buttons:
             self.center_layout.add_widget(self.create_button(screen_manager, button_data))
 
+        # Tambahkan tombol Reload
+        reload_button = Button(
+            text="Reload",
+            size_hint=(1, None),
+            height=50,
+            background_normal='',  # Hilangkan tampilan normal agar latar belakang terlihat
+            background_color=(0, 0, 0, 0),  # Transparan untuk menggunakan latar gradien
+            font_name=FontManager.get_font_name("Regular"),  # Set font Poppins Regular
+            color=(1, 1, 1, 1),  # Optional: Set text color to white for visibility
+        )
+        reload_button.bind(on_release=self.restart_app)
+        self.center_layout.add_widget(reload_button)
+
     def create_button(self, screen_manager, button_data):
         """Membuat tombol dengan teks saja."""
         button = Button(
@@ -84,3 +100,16 @@ class Sidebar(BoxLayout):
         """Navigasi ke layar tertentu."""
         if screen_name in screen_manager.screen_names:
             screen_manager.current = screen_name
+
+    def restart_app(self, dt=None):
+        """Restart aplikasi menggunakan subprocess."""
+        print("Aplikasi akan di-restart...")
+
+        # Menghentikan aplikasi
+        App.get_running_app().stop()
+
+        # Gunakan subprocess untuk menjalankan ulang aplikasi
+        subprocess.Popen([sys.executable, sys.argv[0]])
+
+        # Keluar dari aplikasi yang sedang berjalan
+        sys.exit()
